6b8df1619ab62bbfc0f473411e42a94b163518cd
jide <jide.fr@gmail.com>
2024-01-04 23:22:03 +0100

    repro

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/1.html",
+    "./src/(test)/2.html",
+    "./src/[test]/3.html",
+    "./src/[test]/(test)/4.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
